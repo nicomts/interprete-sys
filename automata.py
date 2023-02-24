efb6383927300159279391dde970e7f937f568f8
nicomts <>
2023-02-24 20:21:27 -0300

    Add identificador automaton

--- automata.py
+++ automata.py
@@ -33,3 +33,36 @@ def es_real(cadena):
         cadena_aceptada = True
         return cadena_aceptada
 
+
+def es_identificador(cadena):
+
+    # rules: identificador always starts with a letter, the rest can be letters or digits, no special symbols (not even _)
+    def caracter_a_simbolo(caracter):
+        es_digito = re.search('[0-9]', caracter)
+        es_letra = re.search('[a-zA-Z]', caracter)
+
+        if es_letra:
+            return 'letra'
+        elif es_digito:
+            return 'digito'
+        else:
+            return 'otro'
+
+    estado_inicial = 0
+    estados_finales = [2]
+
+    control = 0
+    estado_actual = estado_inicial
+
+    delta = {0: {'letra': 2, 'digito': 1, 'otro': 1},
+             1: {'letra': 1, 'digito': 1, 'otro': 1},
+             2: {'letra': 2, 'digito': 2, 'otro': 1}}
+
+    for i in range(len(cadena)):
+        transicion = caracter_a_simbolo(cadena[control])
+        estado_actual = delta[estado_actual][transicion]
+        control += 1
+    if estado_actual in estados_finales:
+        cadena_aceptada = True
+        return cadena_aceptada
+
