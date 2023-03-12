dafb3b07a3e0801e7aec0b5b6b0457c27ae8f668
nicomts <>
2023-03-12 16:51:42 -0300

    Add reserved words to table

--- tabla_de_simbolos.py
+++ tabla_de_simbolos.py
@@ -1,6 +1,14 @@
 
 def crear_tabla(tabla):
     instalar_en_tabla(tabla, 'var', 'var')
+    instalar_en_tabla(tabla, 'read', 'read')
+    instalar_en_tabla(tabla, 'write', 'write')
+    instalar_en_tabla(tabla, 'if', 'if')
+    instalar_en_tabla(tabla, 'else', 'else')
+    instalar_en_tabla(tabla, 'while', 'while')
+    instalar_en_tabla(tabla, 'not', 'not')
+    instalar_en_tabla(tabla, 'and', 'and')
+    instalar_en_tabla(tabla, 'or', 'or')
 
 
 def instalar_en_tabla(tabla, componente_lexico, lexema):
