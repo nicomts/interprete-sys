bb7d6d74056bc73d77633f1ed8b14ae0059eacd5
nicomts <>
2023-09-02 20:03:04 -0300

    Refactor tests

--- main.py
+++ main.py
@@ -1,52 +1,7 @@
-from analizador_sintactico import *
-import analizador_lexico
-import tabla_de_simbolos
-
-def test_analizador_sintactico(ruta_archivo):
-    a = analizador_sintactico(ruta_archivo)
-    arbol_derivacion = a[0]
-    exito = a[1]
-    error = a[2]
-    arbol_derivacion.imprimir()
-    if exito:
-        print('Éxito')
-    if error:
-        print('Error')
-
-
-
-def test_analizador_sintactico_2(ruta_archivo):
-    a = analizador_sintactico(ruta_archivo)
-    arbol_derivacion = a[0]
-    exito = a[1]
-    error = a[2]
-
-    def dfs(nodo):
-        print(nodo.valor)
-        for hijo in nodo.hijos:
-            dfs(hijo)
-
-    dfs(arbol_derivacion)
-
-    if exito:
-        print('Éxito')
-    if error:
-        print('Error')
-
-
-def test_analizador_lexico(ruta_archivo):
-    archivo = open(ruta_archivo)
-    fuente = archivo.read()
-    control_global = 0
-    tabla = []
-    tabla_de_simbolos.crear_tabla(tabla)
-    for i in range(100):
-        aux = analizador_lexico.obtener_siguiente_componente_lexico(fuente, control_global, tabla)
-        control_global = aux[1]
-        print(aux[0] + ': ' + aux[2])
-
+from tests import *
 
 # test_analizador_lexico('test.txt')
 # test_analizador_lexico('test2.txt')
+# test_analizador_sintactico('test2.txt')
 
-test_analizador_sintactico('test2.txt')
+test_analizador_sintactico('suma.txt')
